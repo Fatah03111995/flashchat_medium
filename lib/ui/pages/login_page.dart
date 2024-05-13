1c326059960a2e1c14ca17531b9708e7b5765891
Abdul Fatah <fatah03111995@gmail.com>
2024-05-13 12:50:34 +0700

    feat(login) : design ui done

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:flashchat_medium/ui/components/input_text.dart';
+import 'package:flashchat_medium/ui/style/textstyles.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +10,93 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isPassNotVisible = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage('images/logo.png'),
+                height: 150,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'LOGIN',
+                style: TextStyles.mlBold,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      InputText(
+                        icon: Icons.email_outlined,
+                        controller: emailController,
+                        label: 'e mail',
+                        hint: 'input your e-mail',
+                        isRequired: true,
+                      ),
+                      InputText(
+                        icon: Icons.lock_outline_rounded,
+                        controller: passwordController,
+                        label: 'password',
+                        hint: 'input your password',
+                        isRequired: true,
+                        isNotVisible: isPassNotVisible,
+                        suffix: RawMaterialButton(
+                          elevation: 3,
+                          constraints:
+                              const BoxConstraints(minWidth: 0, minHeight: 0),
+                          // minWidth: double.minPositive,
+                          // height: double.minPositive,
+                          padding: const EdgeInsets.all(5),
+                          shape: const CircleBorder(),
+                          onPressed: () => setState(() {
+                            isPassNotVisible = !isPassNotVisible;
+                          }),
+                          child: isPassNotVisible
+                              ? const Icon(Icons.disabled_visible)
+                              : const Icon(Icons.visibility),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      RawMaterialButton(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 30),
+                        fillColor: Colors.blue,
+                        splashColor: Colors.lightBlue,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            print('work !!');
+                          }
+                        },
+                        child: Text(
+                          'Submit',
+                          style: TextStyles.m,
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
