e78e03130259972f7a6ff881e0c46a3ad49f507b
Abdul Fatah <fatah03111995@gmail.com>
2024-05-15 12:59:17 +0700

    feat : installing and initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ import 'package:flashchat_medium/ui/pages/login_page.dart';
 import 'package:flashchat_medium/ui/pages/registration_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
